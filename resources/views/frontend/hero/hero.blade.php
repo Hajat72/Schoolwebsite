12f7671606ac56e461ae0d81ded9cdc67cbe19b9
Hajat Hussain <hajathussain59@gmail.com>
2026-01-25 16:38:34 +0500

    link hero  section path

--- resources/views/frontend/hero/hero.blade.php
+++ resources/views/frontend/hero/hero.blade.php
@@ -1,13 +1,13 @@
 <!-- Hero Section -->
 <section id="hero" class="hero section dark-background">
 
-    <img src="assets/img/hero-bg.jpg" alt="" data-aos="fade-in">
+    <img src="{{asset('frontend/img/hero-bg.jpg')}}" alt="" data-aos="fade-in">
 
     <div class="container">
         <h2 data-aos="fade-up" data-aos-delay="100">Learning Today,<br>Leading Tomorrow</h2>
         <p data-aos="fade-up" data-aos-delay="200">We are team of talented designers making websites with Bootstrap</p>
         <div class="d-flex mt-4" data-aos="fade-up" data-aos-delay="300">
-            <a href="courses.html" class="btn-get-started">Get Started</a>
+            <a href="#" class="btn-get-started">Get Started</a>
         </div>
     </div>
 
