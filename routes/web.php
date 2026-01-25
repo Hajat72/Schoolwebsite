7bec82833c80d604bd7e3cbdc0696803690a5b01
Hajat Hussain <hajathussain59@gmail.com>
2026-01-25 16:57:33 +0500

    create Website Controller and provide route to  hero section

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WebsiteController;
 
 Route::get('/', function () {
     return view('frontend.layouts.website');
 });
+
+Route::get('/hero',[WebsiteController::class,'hero'])->name('hero-page');
