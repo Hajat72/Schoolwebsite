dd1e0f462d588b8e84ac79f1c85950a45db1a6d8
Hajat Hussain <hajathussain59@gmail.com>
2026-02-05 20:16:02 +0500

    add add Get Register Btn in Header

--- resources/views/frontend/components/header.blade.php
+++ resources/views/frontend/components/header.blade.php
@@ -4,7 +4,7 @@
         <a href="{{route('main-page')}}" class="logo d-flex align-items-center me-auto">
             <!-- Uncomment the line below if you also wish to use an image logo -->
             <!-- <img src="assets/img/logo.png" alt=""> -->
-            <h1 class="sitename">JALALABAD</h1>
+            <h1 class="sitename">BLOOM Rise</h1>
         </a>
 
         <nav id="navmenu" class="navmenu">
@@ -37,7 +37,7 @@
             <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
         </nav>
 
-        <a class="btn-getstarted" href="courses.html">Get Started</a>
+        <a class="btn-getstarted" href="#">Get Register</a>
 
     </div>
 </header>
