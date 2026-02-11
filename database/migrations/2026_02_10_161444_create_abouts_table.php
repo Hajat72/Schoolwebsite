561c802001dd841cb0dc67e1eb3464d872b637e0
Hajat Hussain <hajathussain59@gmail.com>
2026-02-11 17:33:10 +0500

    add status in table

--- database/migrations/2026_02_10_161444_create_abouts_table.php
+++ database/migrations/2026_02_10_161444_create_abouts_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('title')->nullable();
             $table->longText('description')->nullable();
             $table->string('image')->nullable();
+            $table->tinyInteger('status')->nullable();
             $table->timestamps();
         });
     }
